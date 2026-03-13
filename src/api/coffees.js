cb3e25464a2e4f668ee6029814c5d5b314282d84
Pascoal Gomes <pritgomes@gmail.com>
2026-03-13 14:08:33 +1000

    fix: use correct provider API paths (/coffees)

--- src/api/coffees.js
+++ src/api/coffees.js
@@ -1,16 +1,16 @@
 import client from './client';
 
 export const getCoffees = (limit, offset) =>
-  client.get('/coffeesss', { params: { limit, offset } });
+  client.get('/coffees', { params: { limit, offset } });
 
 export const getCoffee = (id) =>
-  client.get(`/coffeesss/${id}`);
+  client.get(`/coffees/${id}`);
 
 export const createCoffee = (data) =>
-  client.post('/coffeesss', data);
+  client.post('/coffees', data);
 
 export const updateCoffee = (id, data) =>
-  client.patch(`/coffeesss/${id}`, data);
+  client.patch(`/coffees/${id}`, data);
 
 export const deleteCoffee = (id) =>
-  client.delete(`/coffeesss/${id}`);
+  client.delete(`/coffees/${id}`);
