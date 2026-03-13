8491a68750faef0de1e73411703761b3e67937ba
Pascoal Gomes <pritgomes@gmail.com>
2026-03-13 14:08:35 +1000

    fix: use correct provider API paths (/users)

--- src/api/users.js
+++ src/api/users.js
@@ -1,16 +1,16 @@
 import client from './client';
 
 export const createUser = (data) =>
-  client.post('/userssxx', data);
+  client.post('/users', data);
 
 export const getUsers = () =>
-  client.get('/userssxx');
+  client.get('/users');
 
 export const getUser = (id) =>
-  client.get(`/userssxx/${id}`);
+  client.get(`/users/${id}`);
 
 export const updateUser = (id, data) =>
-  client.patch(`/userssxx/${id}`, data);
+  client.patch(`/users/${id}`, data);
 
 export const deleteUser = (id) =>
-  client.delete(`/userssxx/${id}`);
+  client.delete(`/users/${id}`);
